051253606afecfe89143ed6dfb786bc027f10ea9
DogeSWL <lusen.w1387@gmail.com>
2017-07-19 02:59:39 -0700

    update heroku

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2:MyNewPass@localhost:8889/build-a-blog2'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://build-a-blog2:MyNewPass@localhost:8889/build-a-blog2')
 
 app.config['SQLALCHEMY_ECHO'] = True
