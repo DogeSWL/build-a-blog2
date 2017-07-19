053620f652a8f9ba1e2034879ca093240e47686e
DogeSWL <lusen.w1387@gmail.com>
2017-07-19 00:42:23 -0700

    modified main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, redirect, flash, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2:MyNewPass@localhost:8889/build-a-blog2'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2:MyNewPass@localhost:8889/build-a-blog2'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://build-a-blog2:MyNewPass@localhost:8889/build-a-blog2')
+
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
